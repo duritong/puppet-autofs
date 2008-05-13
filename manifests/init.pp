9a11a6dc08d2591465bace1c17b4d583398615a0
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 10:57:56 +0200

    init of autofs

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,32 @@
 
 
 # modules_dir { "autofs": }
-class autofs {}
+class autofs {
+    include autofs::base
+}
+
+class autofs::base {
+    # autofs needs nfs
+    include nfs
+
+    package { autofs:
+        ensure => installed,
+    }
+
+    file { "/etc/auto.master":
+        source => [ "puppet://$server/files/autofs/etc/${fqdn}/auto.master",
+                    "puppet://$server/files/autofs/etc/${domain}/auto.master",
+                    "puppet://$server/files/autofs/etc/auto.master",
+                    "puppet://$server/autofs/etc/auto.master.${operatingssystem}",
+                    "puppet://$server/autofs/etc/auto.master" ],
+        notify => Service[autofs],
+        owner => root, group => 0, mode => 0644;
+    }
+
+    service { autofs:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => Package[autofs],
+    }
+}
