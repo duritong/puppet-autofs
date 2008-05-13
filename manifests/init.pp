a5df36f8e9b539e5a8c8907cd5f91c9c0d65ae6c
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 09:58:39 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# autofs module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "autofs": }
+class autofs {}
