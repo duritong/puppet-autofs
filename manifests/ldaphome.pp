e9beaed92a8ed122f0dffc7d966244358e3c1288
Marcel Haerry <haerry@puzzle.ch>
2010-09-27 15:24:24 +0200

    correct new path

--- manifests/ldaphome.pp
+++ manifests/ldaphome.pp
@@ -5,7 +5,7 @@ class autofs::ldaphome {
         source => [ "puppet:///modules/site-autofs/etc/${fqdn}/auto.ldaphome",
                     "puppet:///modules/site-autofs/etc/${domain}/auto.ldaphome",
                     "puppet:///modules/site-autofs/etc/auto.ldaphome",
-                    "puppet:///autofs/etc/auto.ldaphome" ],
+                    "puppet:///modules/autofs/etc/auto.ldaphome" ],
         require => File["/etc/auto.master"],
         notify => Service[autofs],
         owner => root, group => 0, mode => 0644;
