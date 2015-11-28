9a1854f8126317bafc8515bf5c48b072c1c372d9
mh <mh@immerda.ch>
2015-11-28 17:41:06 +0100

    linting for future parser

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,25 +1,28 @@
+# manage basic autofs stuff
 class autofs::base {
   # autofs needs nfs
-  include nfs
+  include ::nfs
 
-  package { autofs:
+  package{'autofs':
     ensure => installed,
   }
 
-  file { "/etc/auto.master":
+  file { '/etc/auto.master':
     source => [ "puppet:///modules/site_autofs/etc/${::fqdn}/auto.master",
                 "puppet:///modules/site_autofs/etc/${::domain}/auto.master",
-                "puppet:///modules/site_autofs/etc/auto.master",
+                'puppet:///modules/site_autofs/etc/auto.master',
                 "puppet:///modules/autofs/etc/auto.master.${::operatingssystem}",
-                "puppet:///modules/autofs/etc/auto.master" ],
-     notify => Service[autofs],
-     owner => root, group => 0, mode => 0644;
+                'puppet:///modules/autofs/etc/auto.master' ],
+    notify => Service['autofs'],
+    owner  => root,
+    group  => 0,
+    mode   => '0644';
   }
 
-  service { autofs:
-    ensure => running,
-    enable => true,
+  service{'autofs':
+    ensure    => running,
+    enable    => true,
     hasstatus => true,
-    require => Package[autofs],
+    require   => Package['autofs'],
   }
 }
--- manifests/ldaphome.pp
+++ manifests/ldaphome.pp
@@ -1,13 +1,15 @@
 # manifests/ldaphome.pp
 class autofs::ldaphome {
   include ::autofs
-  file{"/etc/auto.ldaphome":
-    source => [ "puppet:///modules/site_autofs/etc/${::fqdn}/auto.ldaphome",
+  file{'/etc/auto.ldaphome':
+    source  => [ "puppet:///modules/site_autofs/etc/${::fqdn}/auto.ldaphome",
                 "puppet:///modules/site_autofs/etc/${::domain}/auto.ldaphome",
-                "puppet:///modules/site_autofs/etc/auto.ldaphome",
-                "puppet:///modules/autofs/etc/auto.ldaphome" ],
-    require => File["/etc/auto.master"],
-    notify => Service[autofs],
-    owner => root, group => 0, mode => 0644;
+                'puppet:///modules/site_autofs/etc/auto.ldaphome',
+                'puppet:///modules/autofs/etc/auto.ldaphome' ],
+    require => File['/etc/auto.master'],
+    notify  => Service['autofs'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 }
